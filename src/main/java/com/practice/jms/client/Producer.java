4fbb6a82c733aec09aa61e55556242b4d7186237
Sai Kumar Gummadi <skgummadi@operative.com>
2017-05-27 22:59:49 -0400

    Updated git ignore file
    Updated the git ignore file

---
+++ src/main/java/com/practice/jms/client/Producer.java
@@ -0,0 +1,90 @@
+package com.practice.jms.client;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.naming.NamingException;
+
+import org.apache.activemq.ActiveMQConnection;
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.log4j.BasicConfigurator;
+
+public class Producer {
+
+  private static String url = ActiveMQConnection.DEFAULT_BROKER_URL;
+
+  private static String subject = "MYQUEUE";
+
+  public Producer() throws JMSException, NamingException {
+
+    // Getting JMS connection factory
+
+    ConnectionFactory conFactory = new ActiveMQConnectionFactory(url);
+
+    Connection connection;
+
+    // Getting JMS connection from the server and starting it
+
+    connection = conFactory.createConnection();
+
+    try {
+
+      connection.start();
+
+      // JMS messages are sent and received using a Session. We will
+
+      // create here a non-transactional session object. If you want
+
+      // to use transactions you should set the first parameter to 'true'
+
+      Session session = connection.createSession(false,
+
+          Session.AUTO_ACKNOWLEDGE);
+
+      Destination destination = session.createQueue(subject);
+
+      // MessageProducer is used for sending messages (as opposed
+
+      // to MessageConsumer which is used for receiving them)
+
+      MessageProducer producer = session.createProducer(destination);
+
+      // We will send a small text message saying 'Hello World!'
+
+      TextMessage message = session.createTextMessage("Hello World 1!");
+
+      // Here we are sending the message!
+
+      producer.send(message);
+
+      System.out.println("Sent message '" + message.getText() + "'");
+
+    } finally {
+
+      connection.close();
+
+    }
+
+  }
+
+  public static void main(String[] args) throws JMSException {
+
+    try {
+
+      BasicConfigurator.configure();
+
+      new Producer();
+
+    } catch (NamingException e) {
+
+      e.printStackTrace();
+
+    }
+
+  }
+
+}
